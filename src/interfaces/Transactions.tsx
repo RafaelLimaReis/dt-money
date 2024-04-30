bcd13c71b19986950a2af5f7137b9b2f9d45e350
Rafael Reis <rafaelreis@gurgelmix.com.br>
2024-04-29 21:40:43 -0300

    criando primeira request no json server

--- src/interfaces/Transactions.tsx
+++ src/interfaces/Transactions.tsx
@@ -5,3 +5,12 @@ export interface PriceHighlightProps {
 export interface TransactionTypeButtonPropsInterface {
     variant: 'income' | 'outcome';
 }
+
+export interface TransactionInterface {
+    id: number;
+    description: string;
+    price: number;
+    category: string;
+    type: 'income'|'outcome';
+    createdAt: string;
+}
