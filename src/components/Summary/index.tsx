f9f22afd13a2cf36e717b640c461fcb1b40eea1c
Rafael Reis <rafaelreis@gurgelmix.com.br>
2024-04-29 22:18:37 -0300

    atualizando summary e formatando infos

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,24 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce((summaryAccumulator, transaction) => {
+        if (transaction.type === 'income') {
+            summaryAccumulator.income += transaction.price;
+            summaryAccumulator.total += transaction.price;
+        } else {
+            summaryAccumulator.outcome += transaction.price;
+            summaryAccumulator.total -= transaction.price;
+        }
+        
+        return summaryAccumulator;
+    }, { income: 0, outcome: 0, total: 0 });
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -9,21 +26,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summary.income)}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summary.outcome)}</strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#FFF" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
